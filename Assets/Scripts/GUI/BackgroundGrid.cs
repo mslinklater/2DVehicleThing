16aee089815cefd92321caa898afa462a04c3f20
Martin Linklater <mslinklater@gmail.com>
2018-04-26 21:43:07 +0100

    Removed Grid

---
+++ Assets/Scripts/GUI/BackgroundGrid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Line rendering code taken from the following page: https://docs.unity3d.com/ScriptReference/GL.html
+
+// This component should be attached to a 2D camera GameObject
+
+public class BackgroundGrid : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	void OnPostRender()
+	{
+		
+	}
+}
